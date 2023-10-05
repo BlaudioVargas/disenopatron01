f9ccf7e7876f40e0c2c1841039569d11ac160126
BlaudioVargas <73751250+BlaudioVargas@users.noreply.github.com>
2023-10-05 11:19:31 -0400

    Update proveedor.java

--- proveedor.java
+++ proveedor.java
@@ -1,1 +1,23 @@
+public class proveedor{
+  private String idproveedor;
+  private String direccionproveedor;
+  private String coreeoproveedor;
 
+  public proveedor(String id, String direccion, String correo){
+    this.idproveedor = id;
+    this.direccionproveedor = direccion;
+    this.coreeoproveedor = correo;
+  }
+
+  public String getid(){
+    return this.idproveedor;
+  }
+
+  public String getdireccion(){
+    return this.direccionproveedor;
+  }
+
+  public String getcorreo(){
+    return this.coreeoproveedor;
+  }
+}
