a2123138062cb6cb2e678a988a48a1b01091c01d
BlaudioVargas <73751250+BlaudioVargas@users.noreply.github.com>
2023-10-05 11:49:00 -0400

    Create Inventario.java

---
+++ Inventario.java
@@ -0,0 +1,8 @@
+
+public class Inventario{
+  private Hardware producto;
+  private int stock;//productos disponibles
+  private int ventas;//productos vendidos
+  private int ganancias;//ganancia de ventas
+  
+}
