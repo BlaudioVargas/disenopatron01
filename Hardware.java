566fcaa998bd2022ac41214655bcde7eb8940a36
BlaudioVargas <73751250+BlaudioVargas@users.noreply.github.com>
2023-10-05 11:31:09 -0400

    Update Hardware.java

--- Hardware.java
+++ Hardware.java
@@ -5,4 +5,32 @@ public class Hardware{
   private String modelo;
   private int preciounitario;
   private Proveedor proveedor;
+
+  public class Hardware (String id, String marca, String modelo, int precio, Proveedor proveedor){
+    this.idproducto = id;
+    this.marca = marca;
+    this.modelo = modelo;
+    this.preciounitario = precio;
+    this.proveedor = proveedor;
+  }
+
+  public String getid(){
+    return this.id;
+  }
+
+  public String getmarca(){
+    return this.marca;
+  }
+
+  public String getmodelo(){
+    return this.modelo;
+  }
+
+  public int getprecio(){
+    return this.preciounitario;
+  }
+
+  public Proveedor getproveedor(){
+    return this.proveedor;
+  }
 }
